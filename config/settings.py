533f3604d8e36ce0596bbe56b52b3a9c19df4f49
beyzagunsay <beyza.gunsay@std.yeditepe.edu.tr>
2026-05-11 21:59:05 +0300

    allowed hosts duzeltildi

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3i!ayhzew0v=_6exflyq3xrmuzy9k1sc33!2o!xmq&u4tl@+-i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
